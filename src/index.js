be0cf7810c1268ff31cdfdf08ce6dccb0c198110
PixelMelt <44953835+PixelMelt@users.noreply.github.com>
2024-08-02 14:25:49 -0400

    add basic express server

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors")
+var fs = require("fs");
+const WebSocket = require('ws');
+const path = require('path');
+
+
+const config = require("./config,json")
+const port = config.port;
+
+const app = express();
+
+app.use(express.json());
+app.use(
+  cors()
+);
+
+app.get("/", (req, res) => {
+	res.send("wsp bb")
+})
+
+app.listen(port, () => {
+    console.log(`Server started\nPort ${port}`);
+});
